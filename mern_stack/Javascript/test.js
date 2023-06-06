f0a800ed7fa11dc4b9e1fee5315aa8dd83084cea
Mahdi SAqqa <mahdi.saqqa@gmail.com>
2023-06-07 02:41:58 +0300

    object master

--- mern_stack/Javascript/test.js
+++ mern_stack/Javascript/test.js
@@ -1,17 +1,40 @@
-    // Regular function
-function regularFunction() {
-        console.log(this);
-    };
+let arr = [
+    {
+        name: 'John',
+        age: 20
+    },
+    {
+        name: 'Jane',
+        age: 21
+    },
+    {
+        name: 'Mary',
+        age: 22
+    },
+    {
+        name: 'Mike',
+        age: 23
+    }
+];
+let show = document.getElementById('show');
 
-    // Arrow function
-    const arrowFunction = () => {
-    console.log(this);
-    };
+function cardCreater(i) {
+    let card = `
+    <div class="card" style="width: 18rem;">
+        <img src="https://th.bing.com/th?id=ORMS.c7cdd42818462ae7a6dc63c36ca34ce2&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1&p=0" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${i.name}</h5>
+            <p class="card-text">${i.age}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    `;
+    let temp = new DOMParser().parseFromString(card, "text/xml");
+    console.log(temp);
+    show.append(temp);
+    return temp;
+}
 
-    const obj = {
-    prop: 'Hello',
-    regular: regularFunction,
-    arrow: arrowFunction
-    };
+let cards = arr.map(cardCreater);
+console.log(cards);
 
-obj.arrow();
